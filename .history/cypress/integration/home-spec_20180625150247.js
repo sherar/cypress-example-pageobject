98254c6f61efb7c8e681b45edc2ac576776e9dfa
Gerardo Prieto <sherar@Gerard.local>
2018-06-25 16:20:13 +1200

    Second commit

--- .history/cypress/integration/home-spec_20180625150247.js
+++
@@ -1,9 +0,0 @@
-context('Window', () => {
-    beforeEach(() => {
-        cy.visit('http://google.co.nz')
-    })
-
-    it('Google page is sucessfully displayed', () => {
-        cy.get('[name=btnK]').should('be.visible');
-    })
-})
